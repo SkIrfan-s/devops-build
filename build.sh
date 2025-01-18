e2e5cc5f26c8e001d6ba5a1474c0b25c47f0bb4d
root <root@ip-172-31-41-245.ap-south-1.compute.internal>
2025-01-18 04:31:26 +0000

    commit1

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Building Docker images..."
+docker-compose build
+if [ $? -eq 0 ]; then
+  echo "Docker images built successfully."
+else
+  echo "Failed to build Docker images."
+  exit 1
+fi
+
